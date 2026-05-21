cc064044e2024acb6f1f149a2375ec2100f288e2
Charan <charantejchinnasettyemai@gmail.com>
2026-05-21 17:41:07 +0530

    new changes

--- script.js
+++ script.js
@@ -1,86 +1,131 @@
-// Import the functions you need from the SDKs you need
+// FIREBASE IMPORTS
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
 
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs
 } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// FIREBASE CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyCsiFyyZItNL5r4s_r6QMl3-LEAZuNN6d8",
-  authDomain: "todoapp-5a5b7.firebaseapp.com",
-  projectId: "todoapp-5a5b7",
-  storageBucket: "todoapp-5a5b7.firebasestorage.app",
-  messagingSenderId: "200668242027",
-  appId: "1:200668242027:web:541619e9fd59eb51fd512e",
-  measurementId: "G-110DDKB4VK"
+    apiKey: "AIzaSyCsiFyyZItNL5r4s_r6QMl3-LEAZuNN6d8",
+    authDomain: "todoapp-5a5b7.firebaseapp.com",
+    projectId: "todoapp-5a5b7",
+    storageBucket: "todoapp-5a5b7.firebasestorage.app",
+    messagingSenderId: "200668242027",
+    appId: "1:200668242027:web:541619e9fd59eb51fd512e",
+    measurementId: "G-110DDKB4VK"
 };
 
-// Initialize Firebase
+
+// INITIALIZE FIREBASE
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
+
+
+// HTML ELEMENTS
 const inputBox = document.getElementById("input-box");
+
 const listContainer = document.getElementById("list-container");
-async function saveTaskToFirebase(taskText) {
-    try {
+
+
+// SAVE TASK TO FIREBASE
+async function saveTaskToFirebase(taskText){
+
+    try{
+
         await addDoc(collection(db, "tasks"), {
+
             task: taskText,
+
             createdAt: new Date()
         });
 
         console.log("Task Saved");
-    } catch (error) {
+
+    }
+    catch(error){
+
         console.log(error);
     }
 }
-function addTask(){
+
+
+// ADD TASK
+async function addTask(){
+
     if(inputBox.value === ""){
-        alert("you must be enter something....!");  
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-        saveTaskToFirebase(inputBox.value);
-        saveData();
 
+        alert("Please enter something!");
 
+        return;
     }
+
+    // SAVE TO FIREBASE
+    await saveTaskToFirebase(inputBox.value);
+
+    // CLEAR INPUT
     inputBox.value = "";
+
+    // RELOAD TASKS
+    loadTasks();
 }
 
-    listContainer.addEventListener("click", function(e){
-        if (e.target.tagName === "LI"){
-            e.target.classList.toggle("checked");
-            saveTaskToFirebase(inputBox.value);
-saveData();
 
-        }
-        else if(e.target.tagName ==="SPAN"){
-            e.target.parentElement.remove();
-            saveTaskToFirebase(inputBox.value);
-saveData();
-        }
+// LOAD TASKS FROM FIREBASE
+async function loadTasks(){
 
-    },false);
+    // CLEAR OLD UI
+    listContainer.innerHTML = "";
+
+    // GET FIREBASE DATA
+    const querySnapshot = await getDocs(collection(db, "tasks"));
+
+    querySnapshot.forEach((doc) => {
+
+        const data = doc.data();
+
+        let li = document.createElement("li");
+
+        li.innerHTML = data.task;
+
+        // DELETE BUTTON
+        let span = document.createElement("span");
+
+        span.innerHTML = "\u00d7";
+
+        li.appendChild(span);
+
+        listContainer.appendChild(li);
+    });
+}
 
 
+// CHECK / DELETE
+listContainer.addEventListener("click", function(e){
 
+    // CHECK TASK
+    if(e.target.tagName === "LI"){
 
-    function showData(){
-        listContainer.innerHTML = localStorage.getItem("data");
+        e.target.classList.toggle("checked");
     }
 
-    showData();
+    // DELETE TASK
+    else if(e.target.tagName === "SPAN"){
 
+        e.target.parentElement.remove();
+    }
+
+}, false);
+
+
+// FIRST LOAD AFTER REFRESH
+loadTasks();
+
+
+// GLOBAL FUNCTION
 window.addTask = addTask;
-
